5c2a7056e6b81fd7be3a466e517640df0a13f5cc
Mandarinka450 <nastya-s-01@mail.ru>
2021-06-27 21:58:29 +0300

    add search, pagination and enter info

--- Project/service_job/main/static/script.js
+++ Project/service_job/main/static/script.js
@@ -116,3 +116,5 @@ $(document).ready(function() {
       $('.body__header-burger,.body__menu').toggleClass('active');
   });
 });
+
+
--- Project/service_job/main/views.py
+++ Project/service_job/main/views.py
@@ -7,15 +7,31 @@ def index(request):
     return render(request, 'main/index.html')
 
 def index2(request):
-    vacancies = Vacancies.objects.all()
+    search_query = request.GET.get('search', '')
+    if search_query:
+        vacancies = Vacancies.objects.filter(name_of_vacancy__icontains=search_query)
+    else:
+        vacancies = Vacancies.objects.all()
+    count = Vacancies.objects.all().count()
+
     paginator = Paginator(vacancies, 4)  # 4 поста на каждой странице
-    page = request.GET.get('page')
-    try:
-        page = paginator.page(page)
-    except PageNotAnInteger:
-        # Если страница не является целым числом, поставим первую страницу
-        page = paginator.page(1)
-    except EmptyPage:
-        # Если страница больше максимальной, доставить последнюю страницу результатов
-        page = paginator.page(paginator.num_pages)
-    return render(request, 'main/vacancies.html', {'vacancies': vacancies, 'page': page})
+    page_number = request.GET.get('page', 1)
+    page = paginator.get_page(page_number)
+    is_paginated = page.has_other_pages()
+    if page.has_previous():
+        prev_url = '?page={}'.format(page.previous_page_number())
+    else:
+        prev_url = ''
+    if page.has_next():
+        next_url = '?page={}'.format(page.next_page_number())
+    else:
+        next_url = ''
+    context = {
+        'page_object': page,
+        'is_paginated': is_paginated,
+        'next_url': next_url,
+        'prev_url': prev_url,
+        'count': count,
+    }
+
+    return render(request, 'main/vacancies.html', context=context)
